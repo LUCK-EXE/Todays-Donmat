1cfd878a5c5c270e95a831d694cae267e78d07f6
Silbet <gs1237@kookmin.ac.kr>
2025-11-16 17:30:23 +0900

    feat: Horse의 MonoBehaviour 추가

---
+++ Assets/Scripts/HorseRacing/Horse/Horse.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public HorseData data;
+
+    private float currentSpeed;
+
+    private void Start()
+    {
+        currentSpeed = data.speed;
+    }
+}
