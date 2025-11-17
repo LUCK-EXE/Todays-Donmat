4f47121413cf5d59d2864d8b52a0f9edfe8a77d7
Silbet <gs1237@kookmin.ac.kr>
2025-11-17 21:41:06 +0900

    feat: 말의 자연스러운 움직임 구현
    말의 기본 speed의 값에 +-의 변동값을 줌으로써 말이 자연스럽게 움직이게 함

--- Assets/Scripts/HorseRacing/Horse/Horse.cs
+++ Assets/Scripts/HorseRacing/Horse/Horse.cs
@@ -3,18 +3,20 @@
 public class Horse : MonoBehaviour
 {
     private const float FINISH_LINE_X = 20f;
-    
-    public HorseData data;
 
+    public HorseData data;
+    private HorseMovementPattern movementPattern;
     private float currentSpeed;
 
     private void Start()
     {
         currentSpeed = data.speed;
+        movementPattern = new HorseMovementPattern();
     }
 
     private void Update()
     {
+        currentSpeed = data.speed + movementPattern.GetCurrentPattern();
         if (transform.position.x < FINISH_LINE_X)
         {
             transform.position += Vector3.right * currentSpeed * Time.deltaTime;
