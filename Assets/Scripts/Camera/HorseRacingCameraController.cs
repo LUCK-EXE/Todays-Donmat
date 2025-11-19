952315f4d3bc46687b3421f763f5b3c7f7820fe1
Silbet <gs1237@kookmin.ac.kr>
2025-11-19 13:50:32 +0900

    feat: 경마 카메라 추가
    로직이 구현되지 않은 카메라 추가. 말의 속도와 비슷하게 따라 움직인다.

---
+++ Assets/Scripts/Camera/HorseRacingCameraController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class HorseRacingCameraController : MonoBehaviour
+{
+    private float speed = 3f;
+
+    void Update()
+    {
+        // TODO: 카메라 로직 구현 해야함
+        transform.position += Vector3.right * speed * Time.deltaTime;
+        Debug.Log("카메라 속도: " + speed);
+    }
+}
