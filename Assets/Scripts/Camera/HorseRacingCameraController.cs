6462d1201acf6dfdd2869aafc8eb66e8e65f1478
Silbet <gs1237@kookmin.ac.kr>
2025-11-20 10:36:22 +0900

    feat: 카메라가 선두마를 따라가는 기능 추가
    경마는 1등 결과 확인이 중요하기 때문에 선두마의 시점으로 게임을 진행한다

--- Assets/Scripts/Camera/HorseRacingCameraController.cs
+++ Assets/Scripts/Camera/HorseRacingCameraController.cs
@@ -3,13 +3,16 @@
 public class HorseRacingCameraController : MonoBehaviour
 {
     [SerializeField]
-    private Transform target;
+    private RaceManager raceManager;
     private float smoothTime = 0.3f; // 클수록 느리게, 작을수록 빠르게 카메라가 따라옴
 
     private Vector3 cameraVelocity = Vector3.zero;
 
-    private void LateUpdate() // 말 이동(Update)이후 카메라 이동(LateUpdate)
+    void LateUpdate() // 말 이동(Update)이후 카메라 이동(LateUpdate)
     {
+        Horse leader = raceManager.leaderHorse;
+        Transform target = leader.transform;
+
         Vector3 desiredPosition = new Vector3(
             target.position.x,
             transform.position.y,
