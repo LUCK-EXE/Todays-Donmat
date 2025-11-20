834f9a33f0a91113eaf3af23de6ee244c096cc16
Silbet <gs1237@kookmin.ac.kr>
2025-11-20 12:51:52 +0900

    reafctor: 경마 카메라에서 선두마 위치 가져오는 로직 분리
    기존:
    경마 카메라가 RaceManager을 받아서 선두마를 불러왔음
    
    수정:
    하지만 경마 카메라가 RaceMagaer에 직접 접근하는 것은 카메라의 권한을 넘어서는 일이라 판단해 위치를 반환하는 LeaderPositionProvider클래스 추가.
    
    또한 카메라는 선두마의 전체 정보까진 필요 없음. 선두마의 위치 정보만 받아 쓸 수 있게 수정

--- Assets/Scripts/Camera/HorseRacingCameraController.cs
+++ Assets/Scripts/Camera/HorseRacingCameraController.cs
@@ -3,15 +3,14 @@
 public class HorseRacingCameraController : MonoBehaviour
 {
     [SerializeField]
-    private RaceManager raceManager;
-    private float smoothTime = 0.3f; // 클수록 느리게, 작을수록 빠르게 카메라가 따라옴
+    private LeaderPositionProvider leaderPositionProvider;
 
+    private float smoothTime = 0.3f; // 클수록 느리게, 작을수록 빠르게 카메라가 따라옴
     private Vector3 cameraVelocity = Vector3.zero;
 
     void LateUpdate() // 말 이동(Update)이후 카메라 이동(LateUpdate)
     {
-        Horse leader = raceManager.leaderHorse;
-        Transform target = leader.transform;
+        Transform target = leaderPositionProvider.GetPosition();
 
         Vector3 desiredPosition = new Vector3(
             target.position.x,
