74df5d22b176cda043148905a0e8ded09dd43459
Silbet <gs1237@kookmin.ac.kr>
2025-11-24 03:18:40 +0900

    feat: 경주마 리스트 패널을 동적으로 관리하기 위한 코드 추가

---
+++ Assets/Scripts/HorseRacing/Betting/HorseListPanelController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class HorseListPanelController : MonoBehaviour
+{
+    [SerializeField] private List<HorseData> horseDataList;
+    [SerializeField] private Transform upperRowParent;
+    [SerializeField] private Transform lowerRowParent;
+    [SerializeField] private HorseItemController horseItemPrefab;
+
+    void Start()
+    {
+        int upperRowCount = horseDataList.Count / 2;
+
+        // 경주마가 홀수 일 땐 상단에 1개 적게 배치
+        // 경주마가 짝수 일 땐 상단과 하단에 동일하게 배치
+        for (int i = 0; i < upperRowCount; i++)
+        {
+            var item = Instantiate(horseItemPrefab, upperRowParent);
+            item.Setup(horseDataList[i]);
+        }
+        for (int i = upperRowCount; i < horseDataList.Count; i++)
+        {
+            var item = Instantiate(horseItemPrefab, lowerRowParent);
+            item.Setup(horseDataList[i]);
+        }
+
+    }
+}
