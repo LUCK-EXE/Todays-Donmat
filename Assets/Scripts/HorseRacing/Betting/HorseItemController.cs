7d794e8df9d68941816ad0f36881d0092f699b42
Silbet <gs1237@kookmin.ac.kr>
2025-11-24 17:06:49 +0900

    feat: 배팅할 경주마 선택 버튼 기능 구현

--- Assets/Scripts/HorseRacing/Betting/HorseItemController.cs
+++ Assets/Scripts/HorseRacing/Betting/HorseItemController.cs
@@ -11,14 +11,38 @@ public class HorseItemController : MonoBehaviour
     public TextMeshProUGUI bettingRateText;
     public Button selectButton;
 
-    public void Setup(HorseData horseData)
+    private HorseData horseData;
+    private HorseSelectionManager selectionManager;
+
+    public void Setup(HorseData data, HorseSelectionManager manager)
     {
+        horseData = data;
+        selectionManager = manager;
+
         portraitImage.sprite = horseData.portrait;
         nameText.text = horseData.horseName;
         speedText.text = $"속도: {horseData.speed}";
         descriptionText.text = horseData.description;
         bettingRateText.text = $"배당률: {horseData.bettingRate}배";
+
+        selectButton.onClick.AddListener(OnSelectClicked);
     }
 
-    // TODO: 버튼 기능 구현
+    private void OnSelectClicked()
+    {
+        selectionManager.SelectHorse(this);
+    }
+
+    public HorseData GetHorseData()
+    {
+        return horseData;
+    }
+
+    public void SetSelectedUI(bool isSelected)
+    {
+        // 선택되면 버튼 색 바꾸기
+        ColorBlock cb = selectButton.colors;
+        cb.normalColor = isSelected ? Color.gray : Color.white;
+        selectButton.colors = cb;
+    }
 }
---
+++ Assets/Scripts/HorseRacing/Betting/HorseSelectionManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class HorseSelectionManager : MonoBehaviour
+{
+    // Instantiate 시 코드에서 Add하기
+    public List<HorseItemController> horseItems = new List<HorseItemController>();
+
+    private HorseItemController selectedItem;  // 현재 선택된 카드 정보
+
+    public void SelectHorse(HorseItemController item)
+    {
+        // 기존 선택이 있으면 UI 끔
+        if (selectedItem != null)
+        {
+            selectedItem.SetSelectedUI(false);
+        }
+
+        // 새로운 선택 UI 켬
+        selectedItem = item;
+        selectedItem.SetSelectedUI(true);
+
+        Debug.Log($"경주마 {selectedItem.GetHorseData().horseName}가 선택됨");
+    }
+
+    // 선택된 말을 다른 곳에 전달하는 메소드
+    public HorseData GetSelectedHorse()
+    {
+        if (selectedItem == null)
+            return null;
+
+        return selectedItem.GetHorseData();
+    }
+}
